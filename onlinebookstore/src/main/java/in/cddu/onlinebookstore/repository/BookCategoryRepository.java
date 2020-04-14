713c7c065fd0930c97e2d0b81f5116308af5bf74
mvishwanathan <mvishwanathan@empoweredbenefits.com>
2020-04-14 10:00:40 +0530

    completed the backend for feature one.

--- onlinebookstore/src/main/java/in/cddu/onlinebookstore/repository/BookCategoryRepository.java
+++ onlinebookstore/src/main/java/in/cddu/onlinebookstore/repository/BookCategoryRepository.java
@@ -1,9 +1,11 @@
 package in.cddu.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import in.cddu.onlinebookstore.entity.BookCategory;
 
+@RepositoryRestResource(collectionResourceRel = "bookcategory", path="book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
 
 }
