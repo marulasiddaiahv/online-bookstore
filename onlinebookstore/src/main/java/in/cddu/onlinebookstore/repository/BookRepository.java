4599e9d1fe277ec9e9b1d378aca63122d82db663
mvishwanathan <mvishwanathan@empoweredbenefits.com>
2020-04-19 18:24:29 +0530

    Repository changes

--- onlinebookstore/src/main/java/in/cddu/onlinebookstore/repository/BookRepository.java
+++ onlinebookstore/src/main/java/in/cddu/onlinebookstore/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package in.cddu.onlinebookstore.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import in.cddu.onlinebookstore.entity.Book;
 
+@CrossOrigin("http://localhost:4200")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
